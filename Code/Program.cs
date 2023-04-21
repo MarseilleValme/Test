171d30101427ad665b861dc53a9ac26d49209be1
Grigoriev_Sergey <gsn1232@gmail.com>
2023-04-21 07:57:25 +0300

    add Code

---
+++ Code/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array = { "1234", "167", "-2", "computer science" };
+int array3size = 0, count = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+        array3size++;
+string[] array3 = new string[array3size];
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+    {
+        array3[count] = array[i]; count++;
+    }
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}] → Новый массив: [{string.Join(", ", array3)}]");
